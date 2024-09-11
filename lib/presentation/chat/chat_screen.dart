384c022ffd34363a6b10c4d3ee916a36ea72a2ad
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-11 12:08:09 -0500

    ListView - Area de los Mensajes

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -6,16 +6,45 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      leading: const Padding(
-        padding: EdgeInsets.all(4.0),
-        child: CircleAvatar(
-          backgroundImage: NetworkImage(
-              'https://media.licdn.com/dms/image/v2/D4E03AQFtzgSGEaS1uQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1696455089857?e=1731542400&v=beta&t=ASvBUDVI8jMGH1j8VgrnkQws2E8KpsNC_uw4BAYgr2A'),
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://media.licdn.com/dms/image/v2/D4E03AQFtzgSGEaS1uQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1696455089857?e=1731542400&v=beta&t=ASvBUDVI8jMGH1j8VgrnkQws2E8KpsNC_uw4BAYgr2A'),
+          ),
+        ),
+        title: const Text("Mi amor ❤️"),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  // const _ChatView({
+  //   super.key,
+  // });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            Text('Mundo')
+          ],
         ),
       ),
-      title: const Text("Mi amor ❤️"),
-      centerTitle: false,
-    ));
+    );
   }
 }
