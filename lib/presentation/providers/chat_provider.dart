645d9a909adb4a09b6c0dd32829604585e62d8be
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-11 19:16:21 -0500

    Mover el scroll al final

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messagesList = [
     Message(text: 'Hola', fromWho: FromWho.mine),
     Message(text: 'buenos dias', fromWho: FromWho.mine),
@@ -9,9 +11,19 @@ class ChatProvider extends ChangeNotifier {
   ];
 
   Future<void> sendMessage(String text) async {
-    // if (text.isEmpty) return;
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messagesList.add(newMessage);
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
